1330ccea04dfe604ad7f96ab290460fd8272285b
erofimova <e.erofimova@gmail.com>
2024-11-11 10:17:06 +0200

    Reverse Strings

--- Programming Advanced for QA/Program.cs
+++ Programming Advanced for QA/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+
+while (input != "end")
+{
+    string reversedText = "";
+    for (int position = input.Length - 1; position >= 0; position--)
+    {
+        char curruntSymbol = input[position];
+        reversedText += curruntSymbol;
+    }
+    Console.WriteLine($"{input} = {reversedText}");
+
+    input = Console.ReadLine();
+}
