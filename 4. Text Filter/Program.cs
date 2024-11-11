158db022977f62d887882a48cbd8a8a82817d913
erofimova <e.erofimova@gmail.com>
2024-11-11 23:03:03 +0200

    Text Filter

---
+++ 4. Text Filter/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] badWords = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+
+foreach (string word in badWords)
+{
+    string replacement = new string('*', word.Length);
+    text = text.Replace(word, replacement);
+}
+Console.WriteLine(text);
