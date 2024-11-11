52d68cd6ba86821b709946d32cb0ccbe82d25352
erofimova <e.erofimova@gmail.com>
2024-11-11 10:55:26 +0200

    Substring

---
+++ 3. Substring/Program.cs
@@ -0,0 +1,10 @@
+﻿string word = Console.ReadLine();
+string sentence = Console.ReadLine();
+
+while (sentence.Contains(word))
+{
+    int positionWord = sentence.IndexOf(word);
+    sentence = sentence.Remove(positionWord, word.Length);
+
+}
+Console.WriteLine(sentence);
