ba8defcd195b1b0fc84bef7f68aa0dd9cd0844f0
erofimova <e.erofimova@gmail.com>
2024-11-11 10:37:27 +0200

    Repeat Strings

---
+++ 2. Repeat Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] words = Console.ReadLine().Split(" ");
+string result = "";
+
+foreach (string word in words)
+{
+    int length = word.Length;
+
+    for (int count = 1; count <= length; count++)
+    {
+        result += word;
+    }
+}
+Console.WriteLine(result);
