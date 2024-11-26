3c6498f44a5638fa2aff6bc8667c8c3d45ef4264
erofimova <e.erofimova@gmail.com>
2024-11-26 10:42:21 +0200

    Word Filter

---
+++ 4. Word Filter/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] wordsWithEvenLength = Console.ReadLine().Split(" ").Where(text => text.Length % 2 == 0).ToArray();
+
+foreach (string word in wordsWithEvenLength)
+{
+    Console.WriteLine(word);
+}
