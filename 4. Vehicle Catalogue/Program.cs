5fb13d224f2d419d59d74655bed7c9e48ac56f21
erofimova <e.erofimova@gmail.com>
2024-12-05 00:18:10 +0200

    Vehicle Catalogue

---
+++ 4. Vehicle Catalogue/Program.cs
@@ -0,0 +1,80 @@
+﻿Catalog MyCatalog = new Catalog();
+string[] input = Console.ReadLine().Split('/');
+
+while (input[0] != "end")
+{
+    string typeOfVehicle = input[0];
+    string brand = input[1];
+    string model = input[2];
+
+    if (typeOfVehicle == "Car")
+    {
+        int horsePower = int.Parse(input[3]);
+        Car currentCar = new Car(brand, model, horsePower);
+        MyCatalog.Cars.Add(currentCar);
+
+    }
+    else if (typeOfVehicle == "Truck")
+    {
+        int weight = int.Parse(input[3]);
+        Truck currentTruck = new Truck(brand, model, weight);
+        MyCatalog.Trucks.Add(currentTruck);
+    }
+
+    input = Console.ReadLine().Split('/');
+}
+
+if (MyCatalog.Cars.Count > 0)
+{
+    Console.WriteLine("Cars:");
+    foreach (Car car in MyCatalog.Cars.OrderBy(c => c.Brand))
+    {
+        Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+    }
+}
+if (MyCatalog.Trucks.Count > 0)
+{
+    Console.WriteLine("Trucks:");
+
+    foreach (Truck truck in MyCatalog.Trucks.OrderBy(t => t.Brand))
+    {
+        Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+    }
+}
+class Truck
+{
+    public Truck(string brand, string model, int weight)
+    {
+        Brand = brand;
+        Model = model;
+        Weight = weight;
+
+    }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Weight { get; set; }
+}
+
+class Car
+{
+    public Car(string brand, string model, int horsePower)
+    {
+        Brand = brand;
+        Model = model;
+        HorsePower = horsePower;
+    }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int HorsePower { get; set; }
+}
+
+class Catalog
+{
+    public Catalog()
+    {
+        Cars = new List<Car>();
+        Trucks = new List<Truck>();
+    }
+    public List<Car> Cars { get; set; }
+    public List<Truck> Trucks { get; set; }
+}
